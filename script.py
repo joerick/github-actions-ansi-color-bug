dabcb337f937d005b4e1247c049afab69bad6e65
Joe Rickerby <joerick@mac.com>
2022-12-05 18:56:45 +0000

    Create script.py

---
+++ script.py
@@ -0,0 +1,8 @@
+print("""
+This line is normal
+\033[1m This line is bold
+This line is also bold
+\033[31m this line is red and bold
+This line is red and bold, too \033[0m
+This line is normal again
+""")
