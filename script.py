14c3dea18af88d80d82bf84a184ef2fec1110c32
Joe Rickerby <joerick@mac.com>
2022-12-05 18:58:09 +0000

    Formatting

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 print("""
 This line is normal
-\033[1m This line is bold
+\033[1mThis line is bold
 This line is also bold
-\033[31m this line is red and bold
+\033[31mthis line is red and bold
 This line is red and bold, too \033[0m
 This line is normal again
 """)
